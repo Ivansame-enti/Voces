d320647f7cef7e32503fa00964dad5454c27d2a1
Ivan Sales <ivan.sales@enti.cat>
2022-09-20 21:52:29 +0200

    Animacion arreglada

--- Assets/Proyecto/Scripts/EmergentDialogueController.cs
+++ Assets/Proyecto/Scripts/EmergentDialogueController.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EmergentDialogueController : MonoBehaviour
 {
     private float timer;
     public float timerCD;
+    public Image textBox;
+    public TextMeshProUGUI text;
+    public Sprite goodDialog;
+    public Sprite normalDialog;
+    public Sprite badDialog;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,29 +25,55 @@ public class EmergentDialogueController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space") && timer<=0)
+        if (Input.GetKeyDown(KeyCode.Keypad1) && timer<=0)
         {
-            this.GetComponent<RectTransform>().anchoredPosition = new Vector3(Random.Range(-316.0f, 335.0f), Random.Range(-147.0f, 123.0f), 0);
-            //this.transform.position = new Vector3(Random.Range(-316.0f, 335.0f), Random.Range(-147.0f, 123.0f), 0);
-            //m_RectTransform.anchoredPosition = new Vector2(m_XAxis, m_YAxis);
-            Debug.Log("Hola");
-            //Debug.Log("y: " + this.transform.position.y);
-
-            //this.gameObject.transform.GetChild(0).gameObject.GetComponent<CanvasGroup>().alpha = 1.0f;
-            //this.gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>().Play("Dialogue");
-            //this.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+            //this.gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>().Play("Dialogue", 0, 0.0f);
+            GoodDialogue("Recuperate hijo mio");
             timer = timerCD;
         }
 
-        //Debug.Log(timer);
+        if (Input.GetKeyDown(KeyCode.Keypad2) && timer <= 0)
+        {
+            //this.gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>().Play("Dialogue", 0, 0.0f);
+            NarrativeDialogue("Hoy fui a comprar al super");
+            timer = timerCD;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad3) && timer <= 0)
+        {
+            //this.gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>().Play("Dialogue", 0, 0.0f);
+            BadDialogue("Su hijo puede que no despierte");
+            timer = timerCD;
+        }
+
 
         if (timer >= 0)
         {
             timer -= Time.deltaTime;
-        } else
-        {
-            //this.gameObject.transform.GetChild(0).gameObject.GetComponent<CanvasGroup>().alpha = 0f;
-            //this.gameObject.transform.GetChild(1).gameObject.SetActive(false);
         }
+    }
+
+    public void GoodDialogue(string t)
+    {
+        this.GetComponent<RectTransform>().anchoredPosition = new Vector3(Random.Range(-316.0f, 335.0f), Random.Range(-147.0f, 123.0f), 0);
+        textBox.sprite = goodDialog;
+        text.text = t;
+        this.gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>().Play("Dialogue", 0, 0.0f);
+    }
+
+    public void NarrativeDialogue(string t)
+    {
+        this.GetComponent<RectTransform>().anchoredPosition = new Vector3(Random.Range(-316.0f, 335.0f), Random.Range(-147.0f, 123.0f), 0);
+        textBox.sprite = normalDialog;
+        text.text = t;
+        this.gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>().Play("Dialogue", 0, 0.0f);
+    }
+
+    public void BadDialogue(string t)
+    {
+        this.GetComponent<RectTransform>().anchoredPosition = new Vector3(Random.Range(-316.0f, 335.0f), Random.Range(-147.0f, 123.0f), 0);
+        textBox.sprite = badDialog;
+        text.text = t;
+        this.gameObject.transform.GetChild(0).gameObject.GetComponent<Animator>().Play("Dialogue", 0, 0.0f);
     }
 }
