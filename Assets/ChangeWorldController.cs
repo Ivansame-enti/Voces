c7cd71336cbf3e34d6a6a03d466b0ddfd44679fc
Ivan Sales <ivan.sales@enti.cat>
2022-09-22 18:21:05 +0200

    Efectos implementados, mirar si falta hacer algo mas

--- Assets/ChangeWorldController.cs
+++ Assets/ChangeWorldController.cs
@@ -1,3 +1,4 @@
+using Kino;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,31 +7,50 @@ public class ChangeWorldController : MonoBehaviour
 {
     public GameObject goodWorld;
     public GameObject badWorld;
+    public GameObject mainCamera;
+    private DigitalGlitch dg;
+    private AnalogGlitch ag;
+    private float timer;
+    public float changeCD;
+    private bool goodWorldBool;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        goodWorldBool = true;
+        dg = mainCamera.GetComponent<DigitalGlitch>();
+        ag = mainCamera.GetComponent<AnalogGlitch>();
+        timer = changeCD;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (timer >= changeCD)
         {
-            SendGoodMessage();
-        }
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !goodWorldBool)
+            {
+                StartCoroutine(GoodGlitchEffect());
+                timer = 0;
+                goodWorldBool = true;
+                //SendGoodMessage();
+            }
 
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            SendBadMessage();
+            if (Input.GetKeyDown(KeyCode.LeftControl) && goodWorldBool)
+            {
+                StartCoroutine(BadGlitchEffect());
+                timer = 0;
+                goodWorldBool = false;
+                //SendBadMessage();
+            }
         }
+        else timer += Time.deltaTime;
     }
 
     void SendGoodMessage()
     {
-        goodWorld.SetActive(true);
-        badWorld.SetActive(false);
+        StartCoroutine(GoodGlitchEffect());
+        
         //Debug.Log("Bueno");
     }
 
@@ -39,5 +59,47 @@ public class ChangeWorldController : MonoBehaviour
         goodWorld.SetActive(false);
         badWorld.SetActive(true);
         //Debug.Log("Malo");
+    }
+
+    IEnumerator GoodGlitchEffect()
+    {
+        for (float alpha = 0f; alpha <= 0.7; alpha += 0.1f)
+        {
+            dg.intensity = alpha;
+            ag.scanLineJitter = alpha;
+            ag.verticalJump = alpha;
+            ag.horizontalShake = alpha;
+            ag.colorDrift = alpha;
+            yield return new WaitForSeconds(.1f);
+        }
+
+        dg.intensity = 0;
+        ag.scanLineJitter = 0;
+        ag.verticalJump = 0;
+        ag.horizontalShake = 0;
+        ag.colorDrift = 0;
+        goodWorld.SetActive(true);
+        badWorld.SetActive(false);
+    }
+
+    IEnumerator BadGlitchEffect()
+    {
+        for (float alpha = 0f; alpha <= 0.7; alpha += 0.1f)
+        {
+            dg.intensity = alpha;
+            ag.scanLineJitter = alpha;
+            ag.verticalJump = alpha;
+            ag.horizontalShake = alpha;
+            ag.colorDrift = alpha;
+            yield return new WaitForSeconds(.1f);
+        }
+
+        dg.intensity = 0;
+        ag.scanLineJitter = 0;
+        ag.verticalJump = 0;
+        ag.horizontalShake = 0;
+        ag.colorDrift = 0;
+        goodWorld.SetActive(false);
+        badWorld.SetActive(true);
     }
 }
