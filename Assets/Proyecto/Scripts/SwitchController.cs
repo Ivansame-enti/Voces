808eb677646eaf1e12aace18567ee3932cf3cc3d
Josep <josep.romera@enti.cat>
2022-09-24 00:38:36 +0200

    gitflow-feature-stash: EnvironmentMechanics

---
+++ Assets/Proyecto/Scripts/SwitchController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    private bool kgOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(kgOn == true)
+        {
+            
+        }
+        if(kgOn == false)
+        {
+           
+        }
+        Debug.Log(kgOn);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            kgOn = true;
+        }
+        else if(collision.gameObject.tag == "Box")
+        {
+            kgOn = true;
+        }
+        else
+        {
+            kgOn = false;
+        }
+    }
+
+}
