7796bea1e3645718a7743d7319e0873f08b02865
Ivan Sales <ivan.sales@enti.cat>
2022-09-26 20:30:30 +0200

    Boton acabado y funcionando

---
+++ Assets/PlayerInteraction.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    bool hasPush;
+    public GameObject platform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasPush = false;
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PushButton")
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                Debug.Log("A");
+            }
+        }
+    }*/
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "PushButton")
+        {
+            if (Input.GetKey(KeyCode.Space) && !hasPush)
+            {
+                //Debug.Log("B");
+                platform.SetActive(false);
+                hasPush = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "PushButton")
+        {
+            if (hasPush) hasPush = false;
+        }
+    }
+}
