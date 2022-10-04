ea964864f4d29dcd57f739251d9aa647a76f4c2d
Ivan Sales <ivan.sales@enti.cat>
2022-10-04 20:24:54 +0200

    Se ha seguido con el nivel

--- Assets/MiniLevelController.cs
+++ Assets/MiniLevelController.cs
@@ -8,16 +8,19 @@ public class MiniLevelController : MonoBehaviour
     public SwitchController sw1;
     public SwitchController sw2;
     public SwitchController sw3;
+    private Transform player;
+    public Transform respawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player.position.y < -10.0f) player.position = respawn.position;
         if (sw1.isPressed && sw2.isPressed && sw3.isPressed) SceneManager.LoadScene("Victory");
     }
 }
---
+++ Assets/ObstacleController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    //public Transform camera;
+    public Transform player;
+    public Transform obstacle;
+    public float zoomSpeed;
+    public float distance=20.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //obstacle = player;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 screenPos = this.GetComponent<Camera>().WorldToScreenPoint(player.position);
+        Ray ray = this.GetComponent<Camera>().ScreenPointToRay(screenPos);
+        Debug.DrawRay(ray.origin, ray.direction * 50, Color.yellow);
+
+        RaycastHit hit;
+        //Debug.DrawRay(this.transform.position, camera.position, Color.yellow);
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, distance))
+        {
+            
+            if (hit.collider.gameObject.tag != "Player")
+            {
+                Debug.Log("Holaaa");
+                obstacle.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                obstacle = hit.transform;
+                obstacle.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+                //obstacle.gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+                /*if(Vector3.Distance(obstacle.position, transform.position) >= 3.0f && Vector3.Distance(transform.position, player.position) >= 1.5f)
+                {
+                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
+                }*/
+            } else
+            {
+                Debug.Log("A2");
+                obstacle.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                /*if(Vector3.Distance(transform.position, player.position) >= 1.5f)
+                {
+                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
+                }*/
+            }
+        }
+    }
+}
