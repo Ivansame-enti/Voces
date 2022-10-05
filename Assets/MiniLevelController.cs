2c61099b3d05898a269050331a39cf068177af48
Ivan Sales <ivan.sales@enti.cat>
2022-10-05 10:13:53 +0200

    Acabado

--- Assets/MiniLevelController.cs
+++ Assets/MiniLevelController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,20 +14,27 @@ public class MiniLevelController : MonoBehaviour
     public Transform respawn;
     public GameObject door1, door2;
     private float targetAngle=0.0f, targetAngle2=180.0f;
-
+    public NormalDialogueController ndc;
+    public GameObject camera;
+    public GameObject pingu;
+    private bool flag;
     // Start is called before the first frame update
     void Start()
     {
+        flag = false;
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("a");
-        
         //door2.transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, 1.0f * Time.deltaTime);
-
+        if (ndc.textCount == 3 && !flag)
+        {
+            camera.GetComponent<CinemachineBrain>().enabled = true;
+            pingu.transform.position = new Vector3(pingu.transform.position.x, pingu.transform.position.y, pingu.transform.position.z+3.0f);
+            flag = true;
+        } else if (ndc.textCount != 3) ndc.dialogPlaying = true;
         if (player.position.y < -10.0f) player.position = respawn.position;
         if (sw1.isPressed && sw2.isPressed && sw3.isPressed)
         {
@@ -34,7 +42,7 @@ public class MiniLevelController : MonoBehaviour
             if (targetAngle < 180) targetAngle += 30.0f * Time.deltaTime;
             door1.transform.localRotation = Quaternion.Euler(0, targetAngle, 0);
 
-            if (targetAngle2 > 0) targetAngle2 -= 30.0f * Time.deltaTime;
+            if (targetAngle2 > 5) targetAngle2 -= 30.0f * Time.deltaTime;
             door2.transform.localRotation = Quaternion.Euler(0, targetAngle2, 0);
         } else
         {
--- Assets/PlayerInteraction.cs
+++ Assets/PlayerInteraction.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerInteraction : MonoBehaviour
 {
@@ -25,6 +26,14 @@ public class PlayerInteraction : MonoBehaviour
             }
         }
     }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "End")
+        {
+            SceneManager.LoadScene("Victory");
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
