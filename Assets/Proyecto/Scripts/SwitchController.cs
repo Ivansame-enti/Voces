30fdc22c04680207ea1979972eea705a1766a225
Josep <josep.romera@enti.cat>
2022-10-07 18:19:35 +0200

    Cosas

--- Assets/Proyecto/Scripts/SwitchController.cs
+++ Assets/Proyecto/Scripts/SwitchController.cs
@@ -6,10 +6,12 @@ public class SwitchController : MonoBehaviour
 {
     public bool isPressed;
     private AudioManagerController amc;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         amc = FindObjectOfType<AudioManagerController>();
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,11 +26,13 @@ public class SwitchController : MonoBehaviour
         {
             amc.AudioPlay("Click1");
             isPressed = true;
+            animator.Play("switch");
         }
     }
     private void OnCollisionExit(Collision collision)
     {
         amc.AudioPlay("Click2");
+        animator.Play("switchOff");
         isPressed = false;
     }
 
