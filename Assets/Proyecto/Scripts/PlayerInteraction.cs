59a30eae33061038869bd6a7b11d8cc0f6adc628
Ivan Sales <ivan.sales@enti.cat>
2022-10-09 18:43:23 +0200

    Bastantes arreglos hechos

--- Assets/Proyecto/Scripts/PlayerInteraction.cs
+++ Assets/Proyecto/Scripts/PlayerInteraction.cs
@@ -9,6 +9,7 @@ public class PlayerInteraction : MonoBehaviour
     //public GameObject platform;
     public GameObject box;
     private Vector3 boxPos;
+    public GameObject[] obstacle;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +34,12 @@ public class PlayerInteraction : MonoBehaviour
         {
             SceneManager.LoadScene("Victory");
         }
+
+        if (other.gameObject.tag == "Obstacle")
+        {
+            for(int i=0; i<obstacle.Length;i++)
+                obstacle[i].SetActive(false);
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -41,7 +48,9 @@ public class PlayerInteraction : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.Space) /*&& !hasPush*/)
             {
+                
                 box.SetActive(true);
+                box.GetComponent<Rigidbody>().velocity = new Vector3(0.0f,0.0f,0.0f);
                 box.transform.position = boxPos;
                 //platform.SetActive(false);
                 //hasPush = true;
@@ -51,6 +60,9 @@ public class PlayerInteraction : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        for (int i = 0; i < obstacle.Length; i++)
+            obstacle[i].SetActive(true);
+
         /*if (other.gameObject.tag == "PushButton")
         {
             if (hasPush) hasPush = false;
